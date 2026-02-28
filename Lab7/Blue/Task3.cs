1a553d939f14254b86363ab09ce94f87ffa93602
Евгения <jenekrasilnikova@gmail.com>
2026-02-28 23:29:57 +0300

    Update Task3.cs

--- Lab7/Blue/Task3.cs
+++ Lab7/Blue/Task3.cs
@@ -1,6 +1,72 @@
-﻿namespace Lab7.Blue
+namespace Lab7.Blue
 {
     public class Task3
     {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private int[] _penaltyTimes;
+            
+            public string Name => _name;
+            public string Surname => _surname;
+
+            public int[] PenaltyTimes
+            {
+                get
+                {
+                    int[] copy = new int[_penaltyTimes.Length]; 
+                    Array.Copy(_penaltyTimes, 0,copy,0, copy.Length);
+                    return copy;
+                }
+            }
+            public int TotalTime => _penaltyTimes.Sum();
+
+            public bool IsExpelled
+            {
+                get
+                {
+                    bool isExpelled = true;
+                    if  (_penaltyTimes == null||_penaltyTimes.Length == 0) return !isExpelled;
+                    foreach (var time in _penaltyTimes)//var сам выбирай переменную
+                    {
+                        if (time >=10) return isExpelled;
+                    }
+                    
+                    return !isExpelled;
+                }
+            }
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _penaltyTimes = [];
+            }
+
+            public void PlayMatch(int time)
+            {
+                if (time < 0) return;
+                Array.Resize(ref _penaltyTimes, _penaltyTimes.Length + 1);//ref дай ключи чтобы я изменил адресс в паспорте
+                _penaltyTimes[^1] = time; //^1 последний эл-т
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - 1 - i; j++)
+                    {
+                        if(array[j].TotalTime > array[j + 1].TotalTime)
+                            (array[j], array[j + 1]) = (array[j+1], array[j]);
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                return;
+            }
+        }
     }
 }
